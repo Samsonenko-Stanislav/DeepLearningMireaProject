0586883478375aae4d4971ee534c2ae8df55f61b
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2023-11-03 17:18:13 +0300

    After training

--- data_loader.py
+++ data_loader.py
@@ -9,10 +9,12 @@ def get_cifar10_train_loader(batch_size=4):
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+    batch_size = 4
 
-    return train_loader
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    return trainloader
 
 
 def get_cifar10_test_loader(batch_size=4):
@@ -22,6 +24,6 @@ def get_cifar10_test_loader(batch_size=4):
     ])
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-    test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
 
-    return test_loader
+    return testloader
--- training_process.py
+++ training_process.py
@@ -1,30 +1,40 @@
 import torch
 import torch.optim as optim
+import torch.nn as nn
 import data_loader
 import model
 
-def train_model(model, train_loader, epochs=5, learning_rate=0.001):
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-    for epoch in range(epochs):
+def train_model(net, train_loader):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(2):  # loop over the dataset multiple times
         running_loss = 0.0
         for i, data in enumerate(train_loader, 0):
+            # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
+
+            # zero the parameter gradients
             optimizer.zero_grad()
-            outputs = model(inputs)
+
+            # forward + backward + optimize
+            outputs = net(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
+
+            # print statistics
             running_loss += loss.item()
-            if i % 100 == 99:
-                print(f'Epoch {epoch + 1}, Mini-batch {i + 1}, Loss: {running_loss / 100}')
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
                 running_loss = 0.0
 
-    print('Finished Training')
-    torch.save(model.state_dict(), 'cifar10_model.pth')
+    torch.save(net.state_dict(), 'cifar10_model.pth')
+
 
 if __name__ == "__main__":
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     train_loader = data_loader.get_cifar10_train_loader()
-    cnn_model = model.SimpleCNN()
+    cnn_model = model.Net().to(device)
     train_model(cnn_model, train_loader)
