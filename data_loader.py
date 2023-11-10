ca340584f3800966a07594b07d04a95f3734f5a0
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2023-11-10 17:58:37 +0300

    Added lightning

--- data_loader.py
+++ data_loader.py
@@ -1,29 +1,44 @@
+import os
+
+import lightning as L
 import torch
 import torchvision
-import torchvision.transforms as transforms
 
+from torchvision.datasets import CIFAR10, MNIST
 
-def get_cifar10_train_loader(batch_size=4):
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
+BATCH_SIZE = 256 if torch.cuda.is_available() else 64
 
-    batch_size = 4
+class CIFARDataModule(L.LightningDataModule):
+    def __init__(self, data_dir: str = "./"):
+        super().__init__()
+        self.data_dir = data_dir
+        self.transform = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+        self.dims = (3, 32, 32)
+        self.num_classes = 10
 
-    return trainloader
+    def prepare_data(self):
+        torchvision.datasets.CIFAR10(self.data_dir, train=True, download=True)
+        torchvision.datasets.CIFAR10(self.data_dir, train=False, download=True)
 
+    def setup(self, stage=None):
+        if stage == "fit" or stage is None:
+            cifar_full = torchvision.datasets.CIFAR10(self.data_dir, train=True, transform=self.transform)
+            self.cifar_train, self.cifar_val = torch.utils.data.random_split(cifar_full, [45000, 5000])
+        if stage == "test" or stage is None:
+            self.cifar_test = torchvision.datasets.CIFAR10(self.data_dir, train=False, transform=self.transform)
 
-def get_cifar10_test_loader(batch_size=4):
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(self.cifar_train, batch_size=BATCH_SIZE)
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(self.cifar_val, batch_size=BATCH_SIZE)
 
-    return testloader
+    def test_dataloader(self):
+        return torch.utils.data.DataLoader(self.cifar_test, batch_size=BATCH_SIZE)
--- model.py
+++ model.py
@@ -1,26 +1,60 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch
+import lightning as L
+from torchmetrics.functional import accuracy
 
+from torch.optim.lr_scheduler import StepLR
 
-class Net(nn.Module):
-    def __init__(self):
+class CIFARLitModel(L.LightningModule):
+    def __init__(self, channels, width, height, num_classes, hidden_size=64, learning_rate=2e-2):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.save_hyperparameters()
+
+        self.channels = channels
+        self.width = width
+        self.height = height
+        self.num_classes = num_classes
+        self.hidden_size = hidden_size
+        self.learning_rate = learning_rate
+
+        self.model = nn.Sequential(
+            nn.Conv2d(channels, 6, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(6, 16, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Flatten(),
+            nn.Linear(16 * 5 * 5, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_size, num_classes),
+        )
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1)  # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        return F.log_softmax(self.model(x), dim=1)
 
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        self.log("train_loss", loss, prog_bar=True)
+        return loss
 
-net = Net()
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        acc = accuracy(preds, y, task="multiclass", num_classes=10)
+        self.log("val_loss", loss, prog_bar=True)
+        self.log("val_acc", acc, prog_bar=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.parameters(), lr=self.learning_rate, momentum=0.9)
+        sch = StepLR(optimizer, step_size=1, gamma=0.1)
+        return [optimizer], [sch]
